454c2e0a8549715abdf0701881a4753ea17bc94c
Lucia <lubersol@gmail.com>
2021-05-31 11:43:28 +0200

    signinScreen page created with styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Link, Route} from 'react-router-dom';
 import CartScreen from "./screens/CartScreen";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import SigninScreen from "./screens/SigninScreen";
 
 function App() {
 
@@ -32,6 +33,7 @@ function App() {
       <Route path="/cart/:id?" component={CartScreen}></Route>
       <Route path="/product/:id" component={ProductScreen}></Route>
       <Route path="/" component={HomeScreen} exact></Route>
+      <Route path="/signin" component={SigninScreen}></Route>
       </main>
       <footer className="row center">All right reserved</footer>
     </div>
